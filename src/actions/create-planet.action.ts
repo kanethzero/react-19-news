7333e2e6d052f1d8d8f32227861af5cd045844b3
Lino <kanethplayer@gmail.com>
2025-02-07 23:44:59 -0500

    first commit

---
+++ src/actions/create-planet.action.ts
@@ -0,0 +1,26 @@
+import { planetsApi } from '../api/planetsApi';
+import type { Planet } from '../interfaces/planet.interface';
+
+export const createPlanetAction = async (planet: Partial<Planet>) => {
+    try {
+        const response = await planetsApi.post<Planet>('/', planet);
+        return response.data;
+    } catch (error) {
+        console.log(error);
+        return error;
+    }
+}
+
+export const createPlanetActionForm = async (
+    prevState: unknown,
+    queryData: FormData
+) => {
+    const formData = Object.fromEntries(queryData.entries())
+    try {
+        const response = await planetsApi.post<Planet>('/', formData)
+        return response.data
+    } catch (error) {
+        console.log(error)
+        throw new Error('No se pudo agregar o crear el planeta.')
+    }
+}
---
+++ src/pages/Planets.tsx
@@ -0,0 +1,33 @@
+import { FC, use, useState } from 'react';
+import { Planet } from '../interfaces/planet.interface';
+import { EditPlanetForm } from './ui/EditPlanetForm';
+import { PlanetList } from './ui/PlanetList';
+
+interface Props {
+  getPlanets: Promise<Planet[]>;
+}
+
+const Planets: FC<Props> = ({ getPlanets }) => {
+  const originalPlanets = use(getPlanets)
+  const [planets, setPlanets] = useState<Planet[]>(originalPlanets)
+
+  const handleAddPlanet = async (planet: Planet) => {
+    setPlanets([...planets, planet])
+  };
+
+  console.log(planets, 'dsada')
+
+  return (
+    <>
+      <h4 className="text-2xl font-thin mb-4">Agregar y mantener planetas</h4>
+      <hr className="border-gray-300 mb-4" />
+
+      {/* Formulario para agregar un planeta */}
+      <EditPlanetForm onAddPlanet={handleAddPlanet} />
+
+      <PlanetList planets={planets} />
+    </>
+  );
+};
+
+export default Planets;
